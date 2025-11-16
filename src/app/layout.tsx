b76534855fc881c312391d4d3c6edfd1738bdf70
Lucas Jackson <lucasjackson5815@gmail.com>
2025-11-15 22:48:49 -0800

    update tag line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "CharacterFits",
-  description: "Your personal outfit manager",
+  description: "Be the character you were",
 };
 
 export default function RootLayout({
